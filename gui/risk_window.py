d1b6a747ecbf457307d6f38c6a450b611633ed7a
liaoyi <liaoyizs@163.com>
2020-09-20 22:14:34 +0800

    1. 增加交易弹窗警告，不同意则推出执行。

--- gui/risk_window.py
+++ gui/risk_window.py
@@ -1,3 +1,5 @@
+import sys
+
 from PyQt5 import QtWidgets, Qt
 from gui.risk_gui import Ui_obj_risk
 
@@ -6,3 +8,11 @@ class RiskWindow(QtWidgets.QMainWindow, Ui_obj_risk):
     def __init__(self):
         super(RiskWindow, self).__init__()
         self.setupUi(self)
+
+    def closeEvent(self, event):
+        reply = QtWidgets.QMessageBox.question(self, u'警告', u'同意并继续?',
+                                               QtWidgets.QMessageBox.Yes, QtWidgets.QMessageBox.No)
+        if reply == QtWidgets.QMessageBox.Yes:
+            event.accept()
+        else:
+            sys.exit(-1)
